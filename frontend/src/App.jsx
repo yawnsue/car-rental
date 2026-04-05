7c0effae633775fad063d55b29db87e93a3c330f
Donovan Stanley Jr <160253906+um-idk-tbh@users.noreply.github.com>
2026-04-05 08:34:11 -0400

    Add AdminDashboard component and route
    Any change I/Donovan made should be followed by a comment. So if the code explodes we should be able to easily find the culprit
    
    Signed-off-by: Donovan Stanley Jr <160253906+um-idk-tbh@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,11 @@ import ReservationPage from "./components/ReservationPage";
 import ReservationsOverviewPage from "./components/ReservationsOverviewPage";
 import TripDetailsPage from "./components/TripDetailsPage";
 import AccountPage from "./components/AccountPage";
+import AdminDashboard from './components/AdminDashboard'; // IDonovan added this import
 
 function App()
 {
+  const currentUser = null;// Idonovan added this because locally it would force the login screen first  
   return (
     <Router basename="/car-rental">
       {
@@ -32,6 +34,7 @@ function App()
         <Route path="/reservations" element={<ReservationsOverviewPage />} />
         <Route path="/trip-details/:tripId" element={<TripDetailsPage />} />
         <Route path="/account" element={<AccountPage />} />
+        <Route path="/admin" element={<AdminDashboard />} /> // IDonovan added this route
       </Routes>
     </Router>
   );
